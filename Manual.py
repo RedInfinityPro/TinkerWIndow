2d2644a4658f5fdf6dbe013ca3ef2342fae551bf
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2025-02-13 11:36:13 -0500

    Updating the project contents to replace the old project.

--- Manual.py
+++ Manual.py
@@ -1,40 +1,50 @@
-from customtkinter import CTk, CTkLabel, CTkButton, CTkFrame, CTkScrollbar, CText
+from customtkinter import *
 
-def clear_window():
-    for widget in app.winfo_children():
-        widget.destroy()
+title_font = ("Helvetica", 20, "bold")
+small_font = ("Helvetica", 10)
+small_font_color = ["#2B2B2B", "#E5E5E5"]
+basic_font = ("Helvetica", 11)
 
-def MenuBox(title):
-    menuFrame = MenuTitle(master = app,title = title).grid(row=0, column=0, padx=20, pady=2, sticky="nsew")
-    menuFrame = MenuButtons(master = app).grid(row=1, column=0, padx=20, pady=2, sticky="nsew")
-
-def MenuWindow():
-    clear_window()
-    MenuBox("GUUF TIA")
-
-def BlackmagicManualWindow():
-    clear_window()
-    MenuBox("Blackmagic ATEM Software Control Manual")
-    frame = CTkFrame(master = app)
-    textBox = app.Text(frame)
-
-
-class MenuTitle(CTkFrame):
-    def __init__(self, master, title, **kwargs):
-        super().__init__(master, **kwargs)
-        self.windowTitle = CTkLabel(self, text = title.upper(), font = ("TImes",24,"bold")).pack()
-        
-class MenuButtons(CTkFrame):
+class TopDisplay(CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.menuButton = CTkButton(self, text = "Menu", fg_color="transparent", hover_color="black", border_width=2, border_color="red",command = MenuWindow).grid(row=1, column=0, padx=5)
-        self.blackmagicManual = CTkButton(self, text = "Blackmagic ATEM Software Control Manual", fg_color="transparent", hover_color="black", border_width=2, border_color="red",command = BlackmagicManualWindow).grid(row=1, column=1, padx=5)
-        self.xSplitManual = CTkButton(self, text = "XSplit Manual", fg_color="transparent", hover_color="black", border_width=2, border_color="red").grid(row=1, column=2, padx=5)
-        self.youTubeManual = CTkButton(self, text = "YouTube Manual", fg_color="transparent", hover_color="black", border_width=2, border_color="red").grid(row=1, column=3, padx=5)
-        self.passwordManager = CTkButton(self, text = "Password Manager", fg_color="transparent", hover_color="black", border_width=2, border_color="red").grid(row=1, column=4, padx=5)
+        self.appTitle = CTkLabel(self, text="Welcome",font=title_font).pack(side="top", fill=X, padx=10, pady=10)
+        self.search = CTkEntry(self, placeholder_text="Search...", font=basic_font).pack(fill=X, padx=10, pady=10)
+        
+        self.buttonFrame = CTkFrame(self, height=50, width=100, border_width=1)
+        self.buttonFrame.pack(side="bottom", fill=X, padx=10, pady=10)
 
-app = CTk()
-app.title("Test")
-app.geometry("600x600")
-MenuWindow()
-app.mainloop()
+        self.animalBTN = CTkButton(self.buttonFrame, text="Animals", font=small_font)
+        self.animalBTN.pack(side="left", padx=10, pady=10)
+
+        self.addBTN = CTkButton(self.buttonFrame, text="Add", font=small_font)
+        self.addBTN.pack(side="left", padx=10, pady=10)
+
+        self.plantBTN = CTkButton(self.buttonFrame, text="Plants", font=small_font)
+        self.plantBTN.pack(side="left", padx=10, pady=10)
+
+class BodyDisplay(CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.list = ["Domain","Kingdom","Phylum","Class","Order","Family","Genus","Species"]
+        self.addList = ["Eukarya", "Animalia"]
+        for x, item in enumerate(self.list):
+            self.side_buttonFrame = CTkFrame(self, border_width=1)
+            self.side_buttonFrame.pack(side="left", fill=Y, padx=10, pady=10)
+            self.side_Title = CTkLabel(self.side_buttonFrame, text=item.capitalize(),font=title_font).pack(side="top", fill=X, padx=10, pady=10)
+
+            for i, animal in enumerate(self.addList):
+                self.animal_button = CTkButton(self.side_buttonFrame, text=animal).pack(side="top", fill=X, padx=10, pady=10)
+
+class App(CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Chart")
+        self.geometry("600x600")
+        self.configure(fg_color="#1B1B1B")
+        self.topDisplay = TopDisplay(self, border_width=2).pack(side="top", fill=X, padx=10, pady=10)
+        self.bodyDisplay = BodyDisplay(self, border_width=2).pack(side="top", fill=BOTH, padx=10, pady=10)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
